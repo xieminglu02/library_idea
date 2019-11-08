7d07047757ee0fc7be8527ca210dcc3098f6a62b
xieminglu02 <2986319042@qq.com>
2019-11-08 10:32:46 +0800

    提交到本地仓库

---
+++ ssm/src/com/xieminglu/git/demo1.java
@@ -0,0 +1,13 @@
+package com.xieminglu.git;
+
+/**
+ * @author 迷鹿小女子
+ * @site www.baidu.com
+ * @company xxx公司
+ * @create  2019-11-08 10:29
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("idea git dmeo");
+    }
+}
